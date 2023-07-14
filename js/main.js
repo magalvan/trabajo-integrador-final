65610f61573b57a8849e5c1a9d2805f7aa96c491
Martin Galvan <martin.ale.galvan@gmail.com>
2023-07-14 19:32:00 -0300

    nav

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
-navbar-dark bg-dark text-light">
+navbar-dark bg-primary text-light">
 <div class="container">
 <a class="navbar-brand" href="../templates/index.html">INICIO</a>
 
